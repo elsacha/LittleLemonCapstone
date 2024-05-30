a299f2c2557b3a5706c2898555f05a2c390497b2
alex <admin@littlelemon.com>
2024-05-30 12:27:11 +0200

    Remove duplicate in serializers

--- littlelemon/restaurant/serializers.py
+++ littlelemon/restaurant/serializers.py
@@ -18,8 +18,3 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = "__all__"
-        
-class BookingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Booking
-        fields = "__all__"
